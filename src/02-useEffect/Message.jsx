ab6eaede4c3b2f417c5251e11d2e89ad1e86b694
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-07 01:45:31 -0600

    Custom hook para formularios en FormWithCustomHook.jsx

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,13 +1,20 @@
-import { useEffect } from "react"
+import { useEffect } from 'react';
 
 export const Message = () => {
-    useEffect(() => {
-        console.log('Message Mounted')
+    
+    useEffect(() => {   
+        const onMouseMove = ( {x, y} ) => {
+            const coords = {x, y };
+
+            console.log(coords);
+        }
+
+        window.addEventListener( 'mousemove', onMouseMove );
     
         return () => {
-            console.log('Message Unmounted')
+            window.removeEventListener( 'mousemove', onMouseMove );
         }
-    }, [])
+    }, []);
     
 
     return (
