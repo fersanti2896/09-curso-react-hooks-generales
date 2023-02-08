2ef9f739e8f0ded3b2dbfeef46701e7e88b380d7
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-08 00:23:58 -0600

    Uso del useMemo en MemoHook.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { MemoHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>
 )
