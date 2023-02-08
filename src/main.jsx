a22097de38ba984f196f3fd56af2af7349e6e3d8
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-08 00:05:47 -0600

    Memo como método de React en Small.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,11 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   // </React.StrictMode>
 )
