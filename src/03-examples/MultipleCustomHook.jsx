a8924209392617e796717e654b7e7372ae3904ef
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-07 18:16:18 -0600

    Usando useCounter y useFetch en MultipleCustomHook.jsx

--- src/03-examples/MultipleCustomHook.jsx
+++ src/03-examples/MultipleCustomHook.jsx
@@ -1,11 +1,26 @@
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from './';
 
 export const MultipleCustomHook = () => {
-    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
-
-    console.log({data, isLoading, hasError});
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
+    const { author, quote } = !!data && data[0];
 
     return (
-        <h1>Dr House</h1>
+        <>
+            <h1>Dr House</h1>
+            <hr/>
+
+            { 
+                isLoading ? <LoadingQuote /> 
+                          : <Quote quote={ quote } author={ author } />
+            }  
+
+            <button className='btn btn-success'
+                    disabled={ isLoading }
+                    onClick={ () => increment() }>
+                Next Quote
+            </button>
+        </>
     )
 }
